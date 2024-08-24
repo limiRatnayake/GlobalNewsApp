dd46dc6504b3b68badee0e27f0400788763512a7
Limani Ratnayake <lpw.ratnayake@gmail.com>
2024-08-25 00:15:39 +0530

    firebase integration

--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -1,10 +1,8 @@
 import React, {useState, useEffect} from 'react';
-import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack'; 
+import {NavigationContainer} from '@react-navigation/native'; 
 import MainNavigator from './MainNavigator';
 import AuthNavigator from './AuthNavigator';
-
-const Stack = createNativeStackNavigator();
+ 
 
 const AppNavigator = () => {
   const [user, setUser] = useState(null);
